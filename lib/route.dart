b56f9efa8bf6d90daf91e849e755b5f9ea2d4ebc
wanghao15536870732 <1484290617@qq.com>
2019-02-26 15:04:11 +0800

    first commit.

---
+++ lib/route.dart
@@ -0,0 +1,10 @@
+abstract class UIRoute{
+  //悬浮按钮
+  static const String circleFloatingMenu = 'circle_floating_menu';
+  //练习
+  static const String flutterLake = 'flutter_lake';
+  //单词
+  static const String flutterWord = 'flutter_word';
+  //天气
+  static const String flutterWeather = 'flutter_weather';
+}
